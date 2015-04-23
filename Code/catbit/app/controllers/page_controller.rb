bcf3a6728dbaf43e456c0c32b47c65c8f3294d21
Grant Unruh <grantunruh640@gmail.com>
2015-04-23 16:12:27 -0500

    Started adding html views to rails applicaiton.

---
+++ Code/catbit/app/controllers/page_controller.rb
@@ -0,0 +1,22 @@
+class PageController < ApplicationController
+  def home
+  end
+
+  def water
+  end
+
+  def steps
+  end
+
+  def calories
+  end
+
+  def weight
+  end
+
+  def sleep
+  end
+
+  def miles
+  end
+end
